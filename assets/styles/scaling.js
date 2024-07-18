226039a505d8b36d383e196d95bda2d8c032c576
Christopher Kearl <c2_kearl@yahoo.com>
2024-07-17 22:29:32 -0400

    update scaling functions

--- assets/styles/scaling.js
+++ assets/styles/scaling.js
@@ -15,26 +15,26 @@ const guidelineBaseWidth = () => {
   return 350;
 };
 
+const guidelineBaseHeight = () => {
+    if (isSmall) {
+        return 550;
+    } else if (width > 410) {
+        return 620;
+    }
+    
+    return 680;
+};
+
+const guidelineBaseFonts = () => {
+    if (width > 410) {
+        return 430;
+    }
+    
+    return 400;
+};
+
 const horizontalScale = size => (width / guidelineBaseWidth()) * size;
-
-const guildelineBaseHeight = () => {
-  if (isSmall) {
-    return 550;
-  } else if (width > 410) {
-    return 620;
-  }
-  return 680;
-};
-
-const verticalScale = size => (height / guildelineBaseHeight()) * size;
-
-const guildelineBasaeFonts = () => {
-  if (width > 410) {
-    return 430;
-  }
-  return 400;
-};
-
+const verticalScale = size => (height / guidelineBaseHeight()) * size;
 const scaleFontSize = size => Math.round((width / guidelineBaseWidth()) * size);
 
 export {horizontalScale, verticalScale, scaleFontSize};
