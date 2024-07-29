fbcc386b2f47ab1a4fc82522c6e491e3c95d6903
Christopher Kearl <c2_kearl@yahoo.com>
2024-07-28 22:43:40 -0400

    simplified the dots generation code

--- components/Board/BoardDots.js
+++ components/Board/BoardDots.js
@@ -1,6 +1,36 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
 
+const BOARD_DOTS = {
+  7: [
+    {x: 2, y: 2},
+    {x: 4, y: 2},
+    {x: 2, y: 4},
+    {x: 4, y: 4},
+  ],
+  9: [
+    {x: 2, y: 2},
+    {x: 2, y: 6},
+    {x: 4, y: 4},
+    {x: 6, y: 2},
+    {x: 6, y: 6},
+  ],
+  11: [
+    {x: 3, y: 3},
+    {x: 3, y: 7},
+    {x: 5, y: 5},
+    {x: 7, y: 3},
+    {x: 7, y: 7},
+  ],
+  13: [
+    {x: 3, y: 3},
+    {x: 3, y: 9},
+    {x: 6, y: 6},
+    {x: 9, y: 3},
+    {x: 9, y: 9},
+  ],
+};
+
 function renderDots(cellSize, size) {
   const styles = StyleSheet.create({
     dot: {
@@ -12,106 +42,18 @@ function renderDots(cellSize, size) {
     },
   });
 
-  console.log(`size in renderDots: ${size}`);
-
-  const dots = [];
-
-  if (size === 7) {
-    const dotCoordinates = [
-      {x: 2, y: 2},
-      {x: 4, y: 2},
-      {x: 2, y: 4},
-      {x: 4, y: 4},
-    ];
-
-    const dots = dotCoordinates.map(({x, y}) => (
-      <View
-        key={`dot-${x}-${y}`}
-        style={[
-          styles.dot,
-          {
-            left: x * cellSize - 5,
-            top: y * cellSize - 5,
-          },
-        ]}
-      />
-    ));
-
-    return dots;
-  } else if (size === 9) {
-    for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < 3; j++) {
-        if (
-          (i === 1 && j === 0) ||
-          (i === 0 && j === 1) ||
-          (i === 2 && j === 1) ||
-          (i === 1 && j === 2)
-        ) {
-          continue;
-        }
-        dots.push(
-          <View
-            key={`${i}-${j}`}
-            style={[
-              styles.dot,
-              {
-                left: i * 2 * cellSize + 2 * cellSize - 5,
-                top: j * 2 * cellSize + 2 * cellSize - 5,
-              },
-            ]}
-          />,
-        );
-      }
-    }
-    return dots;
-  } else if (size === 11) {
-    const dotCoordinates = [
-      {x: 2, y: 2},
-      {x: 2, y: 8},
-      {x: 5, y: 5},
-      {x: 8, y: 2},
-      {x: 8, y: 8},
-    ];
-
-    const dots = dotCoordinates.map(({x, y}) => (
-      <View
-        key={`dot-${x}-${y}`}
-        style={[
-          styles.dot,
-          {
-            left: x * cellSize - 5,
-            top: y * cellSize - 5,
-          },
-        ]}
-      />
-    ));
-
-    return dots;
-
-  } else if (size === 13) {
-    const dotCoordinates = [
-      {x: 3, y: 3},
-      {x: 3, y: 9},
-      {x: 6, y: 6},
-      {x: 9, y: 3},
-      {x: 9, y: 9},
-    ];
-
-    const dots = dotCoordinates.map(({x, y}) => (
-      <View
-        key={`dot-${x}-${y}`}
-        style={[
-          styles.dot,
-          {
-            left: x * cellSize - 5,
-            top: y * cellSize - 5,
-          },
-        ]}
-      />
-    ));
-
-    return dots;
-  }
+  return BOARD_DOTS[size].map(({x, y}) => (
+    <View
+      key={`dot-${x}-${y}`}
+      style={[
+        styles.dot,
+        {
+          left: x * cellSize - 5,
+          top: y * cellSize - 5,
+        },
+      ]}
+    />
+  ));
 }
 
 const BoardDots = ({cellSize, boardDimension}) => {
