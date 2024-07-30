633b843f9a5b7c346bd423fef5df8ee1f1f094a8
Christopher Kearl <c2_kearl@yahoo.com>
2024-07-29 20:35:21 -0400

    back button on game.js

--- Game.js
+++ Game.js
@@ -1,13 +1,14 @@
 // Game.js
 
 import React from 'react';
-import {View, Dimensions} from 'react-native';
+import {Button, Dimensions, View} from 'react-native';
 import GameBoard from './components/Board/GameBoard';
 import PieceContainer from './components/Piece/PieceContainer';
 import GameControls from './components/GameControls/GameControls';
 import useGameState from './hooks/useGameState';
 import usePieceMovement from './hooks/usePieceMovement';
 import PieceDish from './components/Board/PieceDish';
+import globalStyles from './assets/styles/globalStyles';
 
 const {width, height} = Dimensions.get('window');
 const boardSize = Math.min(width, height) * 0.9;
@@ -17,7 +18,7 @@ function extractBoardDimension(stringBoardDimension) {
   return parseInt(stringBoardDimension.split('x')[0]);
 }
 
-const Game = ({route}) => {
+const Game = ({route, navigation}) => {
   const boardDimension =
     extractBoardDimension(route.params?.boardDimension) || 9;
   const cellSize = boardSize / (boardDimension - 1);
@@ -40,6 +41,15 @@ const Game = ({route}) => {
 
   return (
     <View style={{flex: 1}}>
+      <View
+        style={globalStyles.gameBackButton}>
+        <Button
+          title="Back"
+          onPress={() => {
+            navigation.navigate('MainMenu');
+          }}
+        />
+      </View>
       <GameBoard
         boardSize={boardSize}
         cellSize={cellSize}
--- assets/styles/globalStyles.js
+++ assets/styles/globalStyles.js
@@ -1,1 +1,14 @@
 // assets/styles/globalStyles.js
+
+import {StyleSheet} from 'react-native';
+
+const globalStyles = StyleSheet.create({
+  gameBackButton: {
+    position: 'absolute',
+    top: 50,
+    left: 10,
+    zIndex: 1,
+  },
+});
+
+export default globalStyles;
